e15de5807bf15e4bd28c781645cb561a1c7c31cc
Saranjib Kuanar <102132535+saranjib@users.noreply.github.com>
2026-09-08 09:18:29 +0530

    Configure Terraform providers for Azure
    Added required providers for azurerm and azuread.

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,1 +1,21 @@
+terraform {
+  required_version = ">= 1.6.0"
 
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "azuread" {}
