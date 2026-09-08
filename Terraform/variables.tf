c70d819d93aad9520f84b79529cb8a47267e8a75
Saranjib Kuanar <102132535+saranjib@users.noreply.github.com>
2026-09-08 09:19:20 +0530

    Add variables for location and resource group name

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,1 +1,12 @@
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "Central India"
+}
+
+variable "resource_group_name" {
+  description = "Resource group name"
+  type        = string
+  default     = "rg-entra-security"
+}
 
