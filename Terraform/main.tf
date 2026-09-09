1e0d0df641784f4e5441e24bf83a39d6bd3c1295
Saranjib Kuanar <102132535+saranjib@users.noreply.github.com>
2026-09-09 11:44:13 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -3,3 +3,21 @@ resource "azurerm_resource_group" "rg-entra-security" {
   name     = var.resource_group_name
   location = var.location
 }
+
+
+resource "azurerm_storage_account" "entra-storage-acc" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group..name
+  location                 = azurerm_resource_group..location
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+
+  min_tls_version           = "TLS1_2"
+
+  public_network_access_enabled = true
+
+  tags = {
+    project     = "entra-id-security"
+    environment = "demo"
+  }
+}
