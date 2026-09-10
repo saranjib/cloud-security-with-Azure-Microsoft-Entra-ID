c327e80a38204ab206fa8be67e75c82d86d5e5a7
Saranjib Kuanar <102132535+saranjib@users.noreply.github.com>
2026-09-10 11:56:12 +0530

    Add role assignments for developers and readers

--- Terraform/rbac.tf
+++ Terraform/rbac.tf
@@ -1,1 +1,11 @@
+resource "azurerm_role_assignment" "developers" {
+  scope                = azurerm_storage_account.entra-storage-acc.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azuread_group.developers.object_id
+}
 
+resource "azurerm_role_assignment" "readers" {
+  scope                = azurerm_storage_account.entra-storage-acc.id
+  role_definition_name = "Reader"
+  principal_id         = azuread_group.readers.object_id
+}
